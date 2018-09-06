186c6a944a89fb8b93c2294f93ea17ee87ded9cb
Francisco Javier Olmos <francisco.olmos2@mavs.uta.edu>
2018-09-06 10:33:16 -0500

    workss

--- sep6/ClassAverage1.java
+++ sep6/ClassAverage1.java
@@ -1,7 +1,7 @@
 // creating a sentinel controlled iteration
 import java.util.Scanner;
 
-public class ClassAverage1;{
+public class ClassAverage1 {
   public static void main(String[] args) {
 
     Scanner input = new Scanner (System.in);
@@ -9,20 +9,26 @@ public class ClassAverage1;{
     int total = 0;
     int gradeCounter = 0;
 
-    system.out.print ("enter grade or -1 to quit");
+    System.out.print ("enter grade or -1 to quit");
     //-1 is the sentinel control
-    int mygrade= input.nextint();
+    int mygrade= input.nextInt();
     //loop
     while (mygrade != -1){
       total = total +mygrade;
-      gradeCounter= gradeCounter+1 //not for the loop, for the division
+      gradeCounter= gradeCounter+1; //not for the loop, for the division
 
       System.out.print ("Enter grade or -1 to quit");
-      mygrade= input.nextint(); // read the input
+      mygrade= input.nextInt(); // read the input
     }//close while
 
     if (gradeCounter !=0)
-    { double average = (double) total/gradeCounter}
-  }
+    { double average = (double) total/gradeCounter;
 
+    System.out.printf("The total is %d, the total number is %d, the average is %.2f%n", total,gradeCounter, average);
+    }//end if
+
+    else{
+      System.out.print("no grades entered");
+    }
+  }//end main
 }
