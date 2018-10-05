d4447ae7c10fa96ffe3d1297901563615ee30340
Francisco Javier Olmos <francisco.olmos2@mavs.uta.edu>
2018-10-05 16:56:53 -0500

    final draft

--- assignment2/SellingPrice.java
+++ assignment2/SellingPrice.java
@@ -23,18 +23,14 @@
      // asking for sale revenue
      System.out.print("Enter sales revenue or -1 to quit:");
      int sale_revenue = input.nextInt();
-
-     if(sale_revenue != -1){
      // promt for total number of products sold
-     System.out.print("Enter number of products sold:");
+     System.out.print("Enter number of products sold or -1 to quit:");
      int products_sold = input.nextInt();
 
      double sell_price = (double) sale_revenue / products_sold; // selling price
      System.out.printf("Selling price for the first year is $%.2f%n",sell_price);
-      }
-      else{
-        break
-      }
+
+
 
      // loop until sentinel values read from user
      while (sale_revenue != -1){
@@ -48,31 +44,31 @@
 
        System.out.print("Enter sales revenue or -1 to quit: ");
        sale_revenue = input.nextInt();
-       while (sale_revenue != -1){
+       if (sale_revenue != -1 ){
        System.out.print("Enter total number of products sold: " ); // promt for number sold
        products_sold = input.nextInt();
 
        sell_price = (double) sale_revenue / products_sold; // selling price
        System.out.printf("Selling price for the second year is $%.2f%n",sell_price);
-     }//
+        }
+      else{
+        sale_revenue = -1;
+      }
+      }// end 1st while
 
-     }// end 2nd while
-   }// end 1st while
+      // and least one year was entered
+      if (year_counter != 0){
 
-     // and least one year was entered
-     if (year_counter != 0){
+     //     double average_selling = (double) combined_selling_price / total_products_sold;
+        double average_selling = (double) total_sales_revenue / total_products_sold;
 
-  //     double average_selling = (double) combined_selling_price / total_products_sold;
-       double average_selling = (double) total_sales_revenue / total_products_sold;
-
-       System.out.printf("Total sales revenue is $%.2f%n", total_sales_revenue);// Total sales revenue
-       System.out.printf("Total number of products sold is %.2f%n", total_products_sold);// Total number of products sold
-       System.out.printf("combined selling price is $%.2f%n", average_selling);// combined selling price
-     }
-     else {
-       System.out.println("No values were entered");
-     }// end if loop
-
+        System.out.printf("Total sales revenue is $%.2f%n", (double)total_sales_revenue);// Total sales revenue
+        System.out.printf("Total number of products sold is %.2f%n", (double)total_products_sold);// Total number of products sold
+        System.out.printf("combined selling price is $%.2f%n", average_selling);// combined selling price
+      }
+      else {
+        System.out.println("No values were entered");
+      }// end if loop
 
 
 
