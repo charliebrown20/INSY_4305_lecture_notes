2de6871f4586f4cd5f91ef15d94769d054f30df0
Francisco Javier Olmos <francisco.olmos2@mavs.uta.edu>
2018-10-05 14:56:30 -0500

    update

--- assignment2/SellingPrice.java
+++ assignment2/SellingPrice.java
@@ -1,1 +1,69 @@
-// SellingPrice
+// SellingPrice.java
+// input sales revenue int
+// input total number of products sold int
+// calculate and display the selling price
+// print total sales revenue
+// total number of products sold
+// combined selling price
+
+ import java.util.Scanner;// importing the scanner class
+
+ public class SellingPrice{
+   public static void main(String[] args) {
+
+     Scanner input = new Scanner (System.in);
+
+     // initializaiton phase
+     int total_sales_revenue = 0;
+     int total_products_sold = 0;
+     int combined_selling_price = 0;
+     int year_counter = 0;
+
+     // processing phase
+     // asking for sale revenue
+     System.out.print("Enter sales revenue or -1 to quit:");
+     int sale_revenue = input.nextInt();
+
+     // promt for total number of products sold
+     System.out.print("Enter number of products sold:");
+     int products_sold = input.nextInt();
+
+     double sell_price = (double) sale_revenue / products_sold; // selling price
+     System.out.printf("Selling price for the first year is $%.2f%n",sell_price);
+
+     
+     // loop until sentinel values read from user
+     while (sale_revenue != -1){
+       total_sales_revenue = total_sales_revenue + sale_revenue;
+       total_products_sold = total_products_sold + products_sold; //total count of product sold
+       combined_selling_price = combined_selling_price + selling_price; // combined price (to be average)
+       year_counter = year_counter + 1; // increment counter
+
+
+       System.out.print("Enter sales revenue or -1 to quit: ");
+       int sale_revenue = input.nextInt();
+
+       System.out.print("Enter total number of products sold: " ) // promt for number sold
+       int products_sold = input.nextInt();
+
+     }// end of while
+
+     // and least one year was entered
+     if (year_counter != 0){
+
+       double average_selling = (double) combined_selling_price / total_products_sold;
+
+       System.out.printf("Total sales revenue is $%.2f%n", total_sales_revenue);// Total sales revenue
+       System.out.printf("Total number of products sold is %.2f%n", total_products_sold);// Total number of products sold
+       System.out.printf("combined selling price is $%.2f%n", average_selling);// combined selling price
+     }
+     else {
+       System.out.println("No values were entered");
+     }// end of if loop
+
+
+
+
+
+   }
+ }
